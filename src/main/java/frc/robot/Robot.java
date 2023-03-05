58af8ef0aef5e23b1f8a88d431ba165589e27170
ultimate_hecker <rohan@halarnkar.com>
2023-03-05 17:17:38 -0500

    new project

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,76 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
 
-  // Configure Right Motors
-  private final WPI_TalonFX rightMotor1 = new WPI_TalonFX(0);
-  private final WPI_TalonFX rightMotor2 = new WPI_TalonFX(0);
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
 
-  // Configure Left Motors
-  private final WPI_TalonFX leftMotor1 = new WPI_TalonFX(0);
-  private final WPI_TalonFX leftMotor2 = new WPI_TalonFX(0);
+  private PWMTalonFX leftMaster = new PWMTalonFX(12);
+  private PWMTalonFX leftSlave = new PWMTalonFX(13);
+  private PWMTalonFX rightMaster = new PWMTalonFX(14);
+  private PWMTalonFX rightSlave = new PWMTalonFX(15);
 
-  // Motor Controller Groups
-  private final MotorControllerGroup rightSlave = new MotorControllerGroup(MotorController[], rightMotor1, rightMotor2);
-  private final MotorControllerGroup leftSlave = new MotorControllerGroup(MotorController[], rightMotor1, rightMotor2);
-
-  DifferentialDrive drivetrain = new DifferentialDrive(rightSlave, leftSlave);
-
-  // Joystick
-  Joystick stick = new Joystick(0);
 
   @Override
   public void robotInit() {}
 
-
   @Override
   public void robotPeriodic() {}
-
 
   @Override
   public void autonomousInit() {}
 
-
   @Override
   public void autonomousPeriodic() {}
-
 
   @Override
   public void teleopInit() {}
 
-
   @Override
   public void teleopPeriodic() {
-    drivetrain.arcadeDrive(stick.getY(), stick.getZ());
+    leftMaster.set(0.5);
+    leftSlave.set(0.5);
+    rightMaster.set(-0.5);
+    rightSlave.set(-0.5);
   }
-
 
   @Override
   public void disabledInit() {}
 
-
   @Override
   public void disabledPeriodic() {}
-
 
   @Override
   public void testInit() {}
 
-
   @Override
   public void testPeriodic() {}
 
-
   @Override
   public void simulationInit() {}
-
 
   @Override
   public void simulationPeriodic() {}
