0d730d4a6368c6eff63f1cdbb960ee1c1eb45aba
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-06 20:25:17 -0500

    added some Utilities

---
+++ src/main/java/frc/robot/Utilities/Drivers/XboxController.java
@@ -0,0 +1,26 @@
+/* 
+
+package frc.robot.Utilities.Drivers;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import static frc.robot.Utilities.Constants.Constants.*;
+
+public class XboxController extends CommandXboxController implements Constants {
+
+    public XboxController = new CommandXboxController(0);
+
+    private static DriveControl instance = null
+    private DriveControl() {
+        if (XboxController.getRightTriggerAxis() > kTriggerAxisThreshold) {
+
+        } else if (XboxController.getRightTriggerAxis() < kTriggerAxisThreshold) {
+            
+        } else if (XboxController.getLeftTriggerAxis() > kTriggerAxisThreshold) {
+            
+        } else if (XboxController.getLeftTriggerAxis() < kTriggerAxisThreshold) {
+            
+        }
+    }
+}
+
+*/
