c5dc613d8b9834501fc4138cf6c21a6e2232bb27
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-13 22:20:05 -0400

    fixed robot overcorrection maybe

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.sensors.SensorVelocityMeasPeriod;
 
 import static frc.robot.Utilities.Constants.Constants.*;
-import frc.robot.subsystems.*;
+// import frc.robot.subsystems.*;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -33,13 +33,49 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
  */
 public class Robot extends TimedRobot {
 
+  public WPI_TalonSRX[] leftMotors;
+  public WPI_TalonSRX[] rightMotors;
+
+  private DifferentialDrive drive;
   private CommandXboxController xbox;
-  private DriveTrain drivetrain;
 
   @Override
   public void robotInit() {
     xbox = new CommandXboxController(0);
-    drivetrain = DriveTrain.getInstance();
+
+    leftMotors = new WPI_TalonSRX[leftMotorCount];
+    leftMotors[0] = new WPI_TalonSRX(leftMasterID);
+    leftMotors[1] = new WPI_TalonSRX(leftSlaveID);
+
+    rightMotors = new WPI_TalonSRX[rightMotorCount];
+    rightMotors[0] = new WPI_TalonSRX(rightMasterID);
+    rightMotors[1] = new WPI_TalonSRX(rightSlaveID);
+
+    for (int i = 0; i < 2; i++) {
+      leftMotors[i].setInverted(true);
+      leftMotors[i].setNeutralMode(NeutralMode.Brake);
+      leftMotors[i].configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+      leftMotors[i].setSelectedSensorPosition(0);
+      leftMotors[i].configVelocityMeasurementWindow(1);
+      leftMotors[i].configVelocityMeasurementPeriod(SensorVelocityMeasPeriod.Period_1Ms);
+
+      rightMotors[i].setInverted(false);
+      rightMotors[i].setNeutralMode(NeutralMode.Brake);
+      rightMotors[i].configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+      rightMotors[i].setSelectedSensorPosition(0);
+      rightMotors[i].configVelocityMeasurementWindow(1);
+      rightMotors[i].configVelocityMeasurementPeriod(SensorVelocityMeasPeriod.Period_1Ms);
+
+      if (i != 0) {
+          leftMotors[i].follow(leftMotors[0]);
+          rightMotors[i].follow(rightMotors[0]);
+      }
+    }
+
+    drive = new DifferentialDrive(
+      new MotorControllerGroup(leftMotors[0], leftMotors[1]),
+      new MotorControllerGroup(rightMotors[0], rightMotors[1])
+    );
   }
 
   @Override
@@ -75,15 +111,15 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {
-    
     if(xbox.getRightTriggerAxis() > kTriggerAxisThreshold) {
+
       double controllerSpeedData = xbox.getRightTriggerAxis();
       double speed = controllerSpeedData * motorReductionSpeed;
 
       double turningData = xbox.getLeftX();
       double turn = turningData * motorReductionTurn;
 
-      double driveRight = speed + turn * motorReductionSpeed;
+      double driveRight = speed + turn * rightSpeedOverCorrection;
       double driveLeft = speed - turn;
 
       leftMotors[0].set(driveLeft);
@@ -92,6 +128,7 @@ public class Robot extends TimedRobot {
       rightMotors[1].set(driveRight);
 
     } else if(xbox.getLeftTriggerAxis() > kTriggerAxisThreshold) {
+
       double controllerSpeedData = xbox.getLeftTriggerAxis();
       double speed = controllerSpeedData * motorReductionSpeed;
 
@@ -107,7 +144,12 @@ public class Robot extends TimedRobot {
       rightMotors[1].set(driveRight);
 
     } else if(xbox.getRightTriggerAxis() < kTriggerAxisThreshold || xbox.getRightTriggerAxis() < kTriggerAxisThreshold) {
-      drive.arcadeDrive(0, 0);
+       
+      leftMotors[0].set(0);
+      leftMotors[1].set(0);
+      rightMotors[0].set(0);
+      rightMotors[1].set(0);
+
     }
   }
 
