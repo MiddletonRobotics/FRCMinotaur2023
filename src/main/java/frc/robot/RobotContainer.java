c87b412da6443fb01797a75b52c96dc865d00a2d
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-16 10:08:58 -0400

    coded command (intake should work)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,31 +2,35 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import frc.robot.Utilities.Drivers.XboxController;
-import frc.robot.commands.ArcadeDrive;
-// import frc.robot.commands.SpinIntakaur;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.commands.ArcadeDrive;
 import frc.robot.subsystems.Intakaur;
-
+import frc.robot.commands.SpinIntakaur;
 import static frc.robot.Utilities.Constants.Constants.*;
 
 public class RobotContainer {
     CommandXboxController DriverController = XboxController.getDriverController();
-
     private final DriveTrain drivetrain = new DriveTrain();
-    private ArcadeDrive ArcadeDrive = new ArcadeDrive(drivetrain);   
-
+    private ArcadeDrive ArcadeDrive = new ArcadeDrive(drivetrain);
+    
+    private final Intakaur intakaur = new Intakaur();
+    private SpinIntakaur SpinIntakaur = new SpinIntakaur(new Intakaur());
+       
     public RobotContainer() {
         configureButtonBindings();
-
-        drivetrain.setDefaultCommand(ArcadeDrive);
+        drivetrain.setDefaultCommand(new RunCommand(() -> ArcadeDrive.execute(), drivetrain));
+        intakaur.setDefaultCommand(new RunCommand(() -> SpinIntakaur.execute(), intakaur));
     }
 
     private void configureButtonBindings() {
         DriverController.leftTrigger().whileTrue(Commands.run(() -> ArcadeDrive.execute()));
-        DriverController.rightTrigger().whileTrue(Commands.run(() -> ArcadeDrive.execute()));     
+        DriverController.rightTrigger().whileTrue(Commands.run(() -> ArcadeDrive.execute()));
+        
+        DriverController.a().whileTrue(Commands.run(() -> SpinIntakaur.execute()));
     }
 }
