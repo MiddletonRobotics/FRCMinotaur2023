a6b8c41789876f8828d942de744bc213692fdac8
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-16 10:01:44 -0400

    simplified command

--- src/main/java/frc/robot/commands/SpinIntakaur.java
+++ src/main/java/frc/robot/commands/SpinIntakaur.java
@@ -1,35 +1,40 @@
-/*
-
 package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import static frc.robot.Utilities.Constants.Constants.*;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-
+import frc.robot.Utilities.Constants.Constants;
 import frc.robot.Utilities.Drivers.XboxController;
 import frc.robot.subsystems.Intakaur;
 
 public class SpinIntakaur extends CommandBase {
     CommandXboxController DriverController = XboxController.getDriverController();
     private final Intakaur intakaur;
-
+    
     public SpinIntakaur(Intakaur intakaur) {
         this.intakaur = intakaur;
         addRequirements(intakaur);
     }
 
-    @Override
-    public void initialize() {}
+    public void initialize(){}
 
-    @Override
-    public void execute() {
-        double intakaurSpeed;
-        intakaur.intakaurMotors[0].set(ControlMode.PercentOutput, intakaurSpeed);
-        intakaur.intakaurMotors[1].set(ControlMode.PercentOutput, intakaurSpeed);
+    public void execute(double value){
+       intakaur.spinMotor(value);
+    }
+
+    public boolean isFinished(){
+        return true;
+    }
+
+    public void end() {
+
+    }
+
+    public void interrupted(){
+        end();
     }
 }
-
-*/
