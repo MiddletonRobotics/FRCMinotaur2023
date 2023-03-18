ff113195f8903b97e1bfde5836678dbc35352b94
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-18 08:40:33 -0400

    coded the arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,11 +13,16 @@ import frc.robot.subsystems.Intakaur;
 import frc.robot.commands.SpinIntakaur;
 // import static frc.robot.Utilities.Constants.Constants.*;
 import frc.robot.commands.Auto;
+import frc.robot.subsystems.Arm;
+import frc.robot.commands.ArmMotion;
 
 import java.util.function.DoubleSupplier;
 
 public class RobotContainer {
     CommandXboxController DriverController = XboxController.getDriverController();
+
+    // Declare Drivetrain 
+
     private final DriveTrain drivetrain = new DriveTrain();
 
     private DoubleSupplier speed = () -> DriverController.getRightTriggerAxis() - DriverController.getLeftTriggerAxis();
@@ -25,14 +30,28 @@ public class RobotContainer {
 
     private ArcadeDrive ArcadeDrive = new ArcadeDrive(drivetrain, speed, rotation);
 
-    private final Auto auto = new Auto(drivetrain);
-    
+    // Declare Arm
+
+    private final Arm arm = new Arm();
+
+    private DoubleSupplier armSpeed = () -> DriverController.getRightY();
+
+    private ArmMotion ArmMotion = new ArmMotion(new Arm(), armSpeed);
+
+    // Declare Intakaur
+
     private final Intakaur intakaur = new Intakaur();
     private SpinIntakaur SpinIntakaur = new SpinIntakaur(new Intakaur());
+
+    // Declare Auto
+
+    private final Auto auto = new Auto(drivetrain);
+    
        
     public RobotContainer() {
         configureButtonBindings();
         drivetrain.setDefaultCommand(ArcadeDrive);
+        arm.setDefaultCommand(ArmMotion);
         intakaur.setDefaultCommand(new RunCommand(() -> SpinIntakaur.execute(), intakaur));
     }
 
